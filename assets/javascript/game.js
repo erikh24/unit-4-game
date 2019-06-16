e70e4acdf7cad56730a8ee6c611915485f51ea90
Erik H <erikjh_24@yahoo.com>
2019-06-15 22:46:30 -0700

    Added buttons to start game and play again. Added variables to game.js for numberWins, numberLosses, yourScore, targetScore and the var for where the random gem values will be stored.

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+// GLOBAL VARIABLE (accessible by all functions)
+//==============================================================================
+
+// Array of all number options for the Target Score (should be between 19 and 120)
+
+// Game Counters
+var numberWins = 0;
+var numberLosses = 0;
+
+// Keeps track of your score as you click getms
+var yourScore;
+
+// Shows the score that you're trying to match
+var targetScore;  
+
+// Gem Values (hidden value to be between 1 and 12)
+var blueValue;
+var greenValue;
+var redValue;
+var yellowValue;
+
