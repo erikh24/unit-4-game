7c63935e175f81d9041dcd7190fcd8b1f03a36d5
Erik H <erikjh_24@yahoo.com>
2019-06-16 18:50:40 -0700

    added on.click events for adding random value of gems to your score. Gems do not currently have a random value to them so nothing will actually be added yet.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,23 @@
 // GLOBAL VARIABLE (accessible by all functions)
 //==============================================================================
 
+
 // Array of all number options for the Target Score (should be between 19 and 120)
+// Might not be needed
 var numberList = []
 
 // Game Counters
 var numberWins = 0;
 var numberLosses = 0;
 
-// Keeps track of your score as you click getms
+// Keeps track of your score as you click gems
 var yourScore;
 
 // Shows the score that you're trying to match
+// Might not be needed
 var targetScore;
+
+
 
 // Gem Values (hidden value to be between 1 and 12)
 var blueValue;
@@ -28,9 +33,6 @@ var yellowValue;
 function targetScore(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
-//  need to remove this line of code once it starts working when calling function startGame
-document.getElementById('target-score').innerHTML = targetScore(19, 120);
-
 
 
 // startGame()
@@ -38,7 +40,37 @@ document.getElementById('target-score').innerHTML = targetScore(19, 120);
 function startGame() {
     // Makes target score show up on the page
     document.getElementById('target-score').innerHTML = targetScore(19, 120);
-
+    
 }
+
+$("#blue").on("click", function() {
+    yourScore += blueValue;
+});
+
+$("#green").on("click", function() {
+    yourScore += greenValue;
+});
+
+$("#red").on("click", function() {
+    yourScore += redValue;
+});
+
+$("#yellow").on("click", function() {
+    yourScore += yellowValue;
+});
+
+// $('#blue').on('click', function (event) {
+//     // code to run after a click has occurred.. 
+//     return Math.floor(Math.random() * (max-mix)) + min;
+//   });
+
+
+//  MAIN PROCESS (THIS IS THE CODE THAT CONTROLS WHAT IS ACTUALLY RUN)
+// ==================================================================================================
+
+// Starts the Game by running the startGame() function
+startGame();
+
+
 
 
